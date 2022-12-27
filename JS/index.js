507e2fcdf4ef26f29308eead7633bf62f3eff293
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2022-12-27 11:53:56 -0300

    feito alteraçao no js

---
+++ JS/index.js
@@ -0,0 +1,15 @@
+let botsobre = document.getElementById("sobre-nos");
+botsobre.addEventListener("click", alerta);
+
+let botinfo = document.getElementById("informativo");
+botinfo.addEventListener("click", alerta);
+
+let bothyundai = document.getElementById("links-hyundai");
+bothyundai.addEventListener("click", alerta);
+
+
+
+function alerta(){
+    window.alert("Em Desenvolvimento");
+}
+    
