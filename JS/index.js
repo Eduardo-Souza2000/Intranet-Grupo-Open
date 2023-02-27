1a6c5bef769bdf2a7e1ef58b819e4833a7460056
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-02-27 17:35:44 -0300

    linquei o convenio ao js

--- JS/index.js
+++ JS/index.js
@@ -7,7 +7,8 @@ botinfo.addEventListener("click", alerta);
 let bothyundai = document.getElementById("links-hyundai");
 bothyundai.addEventListener("click", alerta);
 
-
+let botconvenio = document.getElementById("convenio");
+botconvenio.addEventListener("click", alerta);
 
 function alerta(){
     window.alert("Em Desenvolvimento");
