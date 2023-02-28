1873b7308e9d5eddf0683dd141a4c88841891ef5
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-02-28 15:56:53 -0300

    responsividade na pagina dos ramais

--- JS/ajudaHome.js
+++ JS/ajudaHome.js
@@ -2,6 +2,6 @@ let botAjuda = document.getElementById ("ajudaHome");
 botAjuda.addEventListener("click", alerta);
 
 function alerta(){
-    window.alert("                   Bem Vindo ao Novo Intranet Grupo Open \n Para selecionar uma das opções abaixo, clique em cima dela.\n Após Selecionar a opção desejada será redirecionado para página selecionada.");
+    window.alert("                   Bem Vindo ao Novo Intranet Grupo Open \n\n\n Selecione uma das opções abaixo.\nApós Selecionar a opção desejada será redirecionado para página selecionada.");
 }
 
