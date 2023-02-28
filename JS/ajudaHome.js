0d82aa36bfa675f7ec01408c8378dd7b78c03645
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-02-28 15:22:31 -0300

    feito a inserçao do botao de ajuda na pagina de menu de ramais

---
+++ JS/ajudaHome.js
@@ -0,0 +1,7 @@
+let botAjuda = document.getElementById ("ajudaHome");
+botAjuda.addEventListener("click", alerta);
+
+function alerta(){
+    window.alert("                   Bem Vindo ao Novo Intranet Grupo Open \n Para selecionar uma das opções abaixo, clique em cima dela.\n Após Selecionar a opção desejada será redirecionado para página selecionada.");
+}
+
---
+++ JS/ajudaRamal.js
@@ -0,0 +1,6 @@
+let botAjuda = document.getElementById ("botramal");
+botAjuda.addEventListener("click", alerta);
+
+function alerta(){
+    window.alert(" Após Selecionar a opção desejada será redirecionado para página com a lista de ramal dos colaboradores. \n\n\n Se clicar na logo do Grupo Open ira voltar a pagina Home");
+}
