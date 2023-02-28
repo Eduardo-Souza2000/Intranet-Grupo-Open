62b20efa0e89ae4814a06a7b666de396d1300c69
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-02-28 10:08:47 -0300

    feito o botao de ajuda

---
+++ JS/ajuda.js
@@ -0,0 +1,8 @@
+let botAjuda = document.getElementById("botaoAjuda");
+botAjuda.addEventListener("click", alerta);
+
+
+function alerta(){
+    window.alert(
+        "                   Bem Vindo ao Novo Intranet Grupo Open \n Para selecionar uma das opções abaixo, clique em cima dela.\n Após Selecionar a opção desejada será redirecionado para página selecionada.");
+}
