b30bdbba0afff6d002216d7e72d7589962e6c7b8
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-02-28 09:03:50 -0300

    feito alteraçao na pagina de menu de seleçao dos ramais

--- JS/index.js
+++ JS/index.js
@@ -7,8 +7,20 @@ botinfo.addEventListener("click", alerta);
 let bothyundai = document.getElementById("links-hyundai");
 bothyundai.addEventListener("click", alerta);
 
-let botconvenio = document.getElementById("convenio");
-botconvenio.addEventListener("click", alerta);
+let botmit = document.getElementById("links-mitsubich");
+botmit.addEventListener("click", alerta);
+
+let botren = document.getElementById("links-renault");
+botren.addEventListener("click", alerta);
+
+let botnormas = document.getElementById("normas");
+botnormas.addEventListener("click", alerta);
+
+let botcon = document.getElementById("convenio");
+botcon.addEventListener("click", alerta);
+
+
+
 
 function alerta(){
     window.alert("Em Desenvolvimento");
