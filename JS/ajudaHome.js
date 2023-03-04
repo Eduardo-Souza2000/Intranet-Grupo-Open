a76cfe2f2367b790725d0b3775dc9ae4729c1b25
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-03-04 19:32:43 -0300

    inseri um botão de reconhecimento de voz

--- JS/ajudaHome.js
+++ JS/ajudaHome.js
@@ -2,6 +2,6 @@ let botAjuda = document.getElementById ("ajudaHome");
 botAjuda.addEventListener("click", alerta);
 
 function alerta(){
-    window.alert("                   Bem Vindo ao Novo Intranet Grupo Open \n\n\n Selecione uma das opções abaixo.\nApós Selecionar a opção desejada será redirecionado para página selecionada.");
+    window.alert("                   Bem Vindo ao Novo Intranet Grupo Open \n\n\n O novo Intranet está em fase de Desenvolvimento, estamos somente com a opção de ramais e Ponto mais, disponíveis\n\n Selecione uma das opções abaixo.\nApós Selecionar a opção desejada será redirecionado para página selecionada.");
 }
 
