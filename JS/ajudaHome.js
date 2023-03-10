f95934c88106c6e61e6f75488cc4f983b884ba1a
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-03-10 11:59:02 -0300

    feito o pop na pagina home

--- JS/ajudaHome.js
+++ JS/ajudaHome.js
@@ -1,8 +1,14 @@
-let botAjuda = document.getElementById ("ajudaHome");
-botAjuda.addEventListener("click", alerta);
+// Seleciona o botão de ajuda e o pop-up de ajuda
+var botaoAjuda = document.getElementById("ajudaHome");
+var popUpAjuda = document.getElementById("pop-up-ajuda");
 
-function alerta(){
-    window.alert("                   Bem Vindo ao Novo Intranet Grupo Open \n\n\n O novo Intranet está em fase de Desenvolvimento, estamos somente com a opção de ramais e Ponto mais, disponíveis\n\n Selecione uma das opções abaixo.\nApós Selecionar a opção desejada será redirecionado para página selecionada.");
-}
+// Quando o botão de ajuda é clicado, exibe o pop-up de ajuda
+botaoAjuda.addEventListener("click", function() {
+  popUpAjuda.style.display = "block";
+});
 
-
+// Quando o botão de fechar é clicado, fecha o pop-up de ajuda
+var botaoFechar = document.getElementById("botao-fechar");
+botaoFechar.addEventListener("click", function() {
+  popUpAjuda.style.display = "none";
+});
